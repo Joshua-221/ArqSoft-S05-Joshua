9cab63738a5bae887ae645d2178574a367466f32
Joshua Cruz <joshua.cruz@tecdesoftware.edu.mx>
2026-06-16 20:08:35 -0600

    agregar archivos del proyecto

--- CitasApp/Models/ErrorViewModel.cs
+++ CitasApp/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace CitasApp.Models;
-
-public class ErrorViewModel
+namespace CitasApp.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
