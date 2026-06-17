05bd0a4484029c73fa7d782828454ba7c2d357b3
Joshua Cruz <joshua.cruz@tecdesoftware.edu.mx>
2026-06-16 21:43:49 -0600

    feat: interfaces y diseño

--- CitasApp/obj/Debug/net10.0/CitasApp.AssemblyInfo.cs
+++ CitasApp/obj/Debug/net10.0/CitasApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cab63738a5bae887ae645d2178574a367466f32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84e6a570d8ce49350a7fa47e76657daaf9b4c64b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
